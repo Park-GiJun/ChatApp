8b9dd8776cd13c3f10f03a54f312556fcbf20ba9
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-26 16:01:54 +0900

    inti

--- src/com/chat/Main.java
+++ src/com/chat/Main.java
@@ -1,16 +1,10 @@
 package com.chat;
 
 public class Main {
-	
 	public Main() {
-		
 	}
-	
 	public static void main(String[] args) {
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.setVisible(true);
 	}
-	
-	
-
 }

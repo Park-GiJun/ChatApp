b4acb96d931f7d0aa953b026fb11f43cb730c30c
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-26 15:29:33 +0900

    init

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,3 @@
-package com.chat;
-
-
 
 import java.awt.CardLayout;
 import java.awt.Color;
@@ -16,43 +13,49 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTree;
+import javax.swing.plaf.basic.BasicSplitPaneUI.BasicVerticalLayoutManager;
 
 public class MainFrame extends JFrame {
 
-	//로그인 패널
+	// 로그인 패널
 	JPanel loginPanel = new JPanel();
 	JTextField id_TextField = new JTextField();
 	JTextField pwd_TextField = new JTextField();
 	JButton login_Btn = new JButton();
 
-	
-	//메인패널
+	// 메인패널
 	JPanel main_Panel = new JPanel();
 	JPanel left_Panel = new JPanel();
 	JButton home_Btn = new JButton();
 	JButton search_Btn = new JButton();
 	JButton message_Btn = new JButton();
-	
+
 	CardLayout panelLayout = new CardLayout();
 	JPanel card_Panel = new JPanel();
-	
-	//HOME 패널
+
+	// HOME 패널
 	JPanel home_Panel = new JPanel();
 	JPanel home_photo = new JPanel();
 	JLabel home_name = new JLabel();
 	JLabel home_num = new JLabel();
 	JPanel home_todo = new JPanel();
 	JLabel home_todo_list = new JLabel();
-	
-	//서치 패널
+
+	// 서치 패널
 	JPanel search_Panel = new JPanel();
 	JPanel search_List = new JPanel();
 	JPanel search_Page = new JPanel();
 	JTextField search_bar = new JTextField();
 	JTree search_Tree = new JTree();
-	
-	
-	
+
+	// 메세지 패널
+	JPanel message_Panel = new JPanel();
+	JPanel message_Box = new JPanel();
+	JPanel message_chatBox = new JPanel();
+	JButton message_postBtn = new JButton();
+	JPanel message_sendPanel = new JPanel();
+	JTextField message_sendBox = new JTextField();
+	JButton message_sendBtn = new JButton();
 
 	public MainFrame() {
 		CardLayout mainLayout = new CardLayout();
@@ -71,8 +74,8 @@ public class MainFrame extends JFrame {
 		id_TextField.setBounds(218, 326, 251, 38);
 		pwd_TextField.setBounds(218, 374, 251, 38);
 		login_Btn.setBounds(496, 326, 86, 86);
-		
-		//로그인 버튼
+
+		// 로그인 버튼
 		login_Btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				mainLayout.show(getContentPane(), "mainPanel");
@@ -86,22 +89,25 @@ public class MainFrame extends JFrame {
 		main_Panel.setLayout(null);
 		main_Panel.add(left_Panel);
 		left_Panel.setBounds(0, 0, 100, 300);
-		left_Panel.setLayout(new GridLayout(3,1));
+		left_Panel.setLayout(new GridLayout(3, 1));
 		left_Panel.add(home_Btn);
+		home_Btn.setSize(100, 100);
 		left_Panel.add(search_Btn);
+		search_Btn.setSize(100, 100);
 		left_Panel.add(message_Btn);
-		
-		//변경 패널
-		
+		message_Btn.setSize(100, 100);
+
+		// 변경 패널
+
 		main_Panel.add(card_Panel);
 		card_Panel.setLayout(panelLayout);
 		card_Panel.setBounds(100, 0, 700, 560);
 		card_Panel.setBackground(Color.red);
 		card_Panel.add(home_Panel, "homePanel");
 		card_Panel.add(search_Panel, "searchPanel");
-		
-		
-		//홈패널
+		card_Panel.add(message_Panel, "messagePanel");
+
+		// 홈패널
 		home_Panel.setLayout(null);
 		home_Panel.setBackground(Color.gray);
 		home_Panel.setBounds(100, 0, 700, 560);
@@ -114,19 +120,45 @@ public class MainFrame extends JFrame {
 		home_Panel.add(home_todo);
 		home_todo.setBounds(195, 356, 310, 170);
 		home_todo.add(home_todo_list);
-		
-		//서치패널
-		search_Panel.setBackground(Color.green);
-		search_Page.setLayout(null);
-		search_Panel.setBounds(100, 0, 700, 560);
+
+		// 서치패널
+		search_Panel.setLayout(null);
+		search_Panel.setBounds(0, 0, 700, 560);
 		search_Panel.add(search_List);
+		search_List.setBackground(Color.darkGray);
 		search_List.setBounds(0, 0, 230, 560);
 		search_Panel.add(search_Page);
-		search_Page.setBounds(330, 0, 470, 560);
+		search_Page.setBounds(230, 0, 470, 560);
+		search_Page.setBackground(Color.green);
 		search_List.add(search_bar);
+		search_bar.setBounds(0, 28, 200, 50);
 		search_List.add(search_Tree);
-		
-		
+		search_List.setBounds(0, 81, 230, 450);
+
+		// 메세지 패널
+		message_Panel.setLayout(null);
+		message_Panel.setBounds(100, 0, 700, 560);
+		message_Panel.setBackground(Color.blue);
+		message_Panel.add(message_Box);
+		message_Box.setBounds(0, 0, 90, 560);
+		message_Box.setBackground(Color.orange);
+		message_Box.setLayout(null);
+		message_Panel.add(message_chatBox);
+		message_chatBox.setBounds(90, 0, 610, 560);
+		message_chatBox.setBackground(Color.pink);
+		message_Box.add(message_postBtn);
+		message_postBtn.setBounds(0, 0, 90, 70);
+		message_chatBox.add(message_sendBox);
+		message_chatBox.setLayout(null);
+		message_chatBox.add(message_sendPanel);
+		message_sendPanel.setLayout(null);
+		message_sendPanel.setBounds(0, 485, 610, 40);
+		message_sendPanel.add(message_sendBox);
+		message_sendBox.setBounds(0, 0, 520, 40);
+		message_sendPanel.add(message_sendBtn);
+		message_sendBtn.setBounds(520,0,90,40);
+
+
 		home_Btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				panelLayout.show(card_Panel, "homePanel");
@@ -137,10 +169,14 @@ public class MainFrame extends JFrame {
 				panelLayout.show(card_Panel, "searchPanel");
 			}
 		});
-		
-		
-		
-		
+
+		message_Btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelLayout.show(card_Panel, "messagePanel");
+
+			}
+		});
+
 	}
 
 }
